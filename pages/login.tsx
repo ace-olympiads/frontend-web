4cec51231d94dd9d70d1e8b2d0500c4eb0cdc55c
Sahajpreet Singh <sp.code2003@gmail.com>
2023-06-06 02:40:16 +0530

    feat: add login page

---
+++ pages/login.tsx
@@ -0,0 +1,121 @@
+import { useCallback, useState } from "react";
+import { BsGoogle, BsFacebook, BsInstagram } from "react-icons/bs";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import SocialAuthButton from "../components/SocialAuthButton";
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const LoginPage: React.FC = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const toggleVariant = useCallback(() => {
+    setVariant((prevVariant) =>
+      prevVariant === "LOGIN" ? "REGISTER" : "LOGIN"
+    );
+  }, []);
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    // Handle form submission
+  };
+
+  const socialAction = (action: string) => {
+    // Handle social authentication
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="email"
+            label="Email address"
+            type="email"
+          />
+          {variant === "REGISTER" && (
+            <Input
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id="username"
+              label="Name"
+            />
+          )}
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="password"
+            label="Password"
+            type="password"
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <SocialAuthButton
+              icon={BsFacebook}
+              onClick={() => socialAction("facebook")}
+            />
+            <SocialAuthButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+            <SocialAuthButton
+              icon={BsInstagram}
+              onClick={() => socialAction("instagram")}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant === "LOGIN"
+              ? "New to AceOlympiads?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
