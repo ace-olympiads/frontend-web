1213df3b3f5e42e5dbb0d4d1b7649f3257c5c988
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2023-06-27 11:53:16 +0530

    chore: remove unused components

--- components/VideoComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Video } from "../types";
-const VideoComponent: React.FC<Video[]> = ({ videos }) => {
-  return <div></div>;
-};
-
-export default VideoComponent;
