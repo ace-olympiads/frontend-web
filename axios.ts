a12ab1d189f85da2411f3ca3341209e7ebd7ad99
Ommanimesh2 <ommanimeshmishra@gmail.com>
2023-10-06 09:50:15 +0530

    fix

--- axios.ts
+++ axios.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+import http from "http"; // Import the 'http' module
+
 const baseURL = `${process.env.NEXT_PUBLIC_BACKEND_URL}`;
 const axiosInstance = axios.create({
   baseURL: baseURL,
   headers: { "Content-Type": "application/json" },
+  httpAgent: new http.Agent({ keepAlive: true }), // Use the 'http' Agent for HTTP requests
 });
 
 export default axiosInstance;
